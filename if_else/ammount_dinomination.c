18b95c7d61c65db8ad2b6fe69eee7979cd53dfc3
VikramGujar <vikramgujar300@gmail.com>
2024-07-24 19:10:49 +0530

    commit on 24

--- if_else/ammount_dinomination.c
+++ if_else/ammount_dinomination.c
@@ -12,13 +12,23 @@ int main()
     puts("Enter ammount");
     scanf("%d", &ammount);
 
-    printf("Rs. 500 notes: %d \n", ammount / 500, ammount = ammount % 500);
-    printf("Rs. 200 notes: %d \n", ammount / 200, ammount = ammount % 200);
-    printf("Rs. 100 notes: %d \n", ammount / 100, ammount = ammount % 100);
-    printf("Rs. 50 notes: %d \n", ammount / 50, ammount = ammount % 50);
-    printf("Rs. 20 notes: %d \n", ammount / 20, ammount = ammount % 20);
-    printf("Rs. 10 notes: %d \n", ammount / 10, ammount = ammount % 10);
-    printf("Rs. 5 notes: %d \n", ammount / 5, ammount = ammount % 5);
-    printf("Rs. 2 notes: %d \n", ammount / 2, ammount = ammount % 2);
-    printf("Rs. 1 notes: %d \n", ammount / 1, ammount = ammount % 1);
+
+if(ammount/500!=0)    printf("Rs. 500 notes: %d \n", ammount / 500);
+ammount=ammount%500;
+if(ammount/200!=0)    printf("Rs. 200 notes: %d \n", ammount / 200);
+ammount=ammount%200;
+if(ammount/100!=0)    printf("Rs. 100 notes: %d \n", ammount / 100);
+ammount=ammount%100;
+if(ammount/50!=0)    printf("Rs. 50 notes: %d \n", ammount / 50);
+ammount=ammount%50;
+if(ammount/20!=0)    printf("Rs. 20 notes: %d \n", ammount / 20);
+ammount=ammount%20;
+if(ammount/10!=0)    printf("Rs. 10 notes: %d \n", ammount / 10);
+ammount=ammount%10;
+if(ammount/5!=0)    printf("Rs. 5 notes: %d \n", ammount / 5);
+ammount=ammount%5;
+if(ammount/2!=0)    printf("Rs. 2 notes: %d \n", ammount / 2);
+ammount=ammount%2;
+if(ammount/1!=0)    printf("Rs. 1 notes: %d \n", ammount / 1);
+
 }
